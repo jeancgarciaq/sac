a14be7031aba296b2f08959fc76973eaae8537a0
jeancgarciaq <jcvictory@hotmail.com>
2024-10-05 10:01:10 -0400

    Reorganizacion de los directorios del proyecto

--- Inc/head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Sistema Administrativo de Condominios</title>
-<link rel="stylesheet" href="Css/bulma.min.css">
-<link rel="stylesheet" href="Css/estilos.css">
--- Scripts/session_start.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_name("victory");
-session_start();
-
-?>
