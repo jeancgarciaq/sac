6ba7ccb1a470b771715d984cf2b18b4f321c489f
jeancgarciaq <jcvictory@hotmail.com>
2024-10-13 18:44:28 -0400

    feat: add url to sweetalert

--- app/Views/Inc/head.php
+++ app/Views/Inc/head.php
@@ -1,5 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo APP_NAME; ?></title>
-<link rel="stylesheet" href="css/bulma.min.css">
-<link rel="stylesheet" href="css/estilos.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/bulma.min.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/estilos.css">
+<!--<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>-->
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/sweetalert2.min.css">
+<script src="<?php echo APP_URL; ?>app/views/js/sweetalert2.all.min.js"></script>
