7aa6c494583b7d4671da00a2fa39624b275a26c7
jeancgarciaq <jcvictory@hotmail.com>
2024-10-13 17:56:40 -0400

    fix: put the semicolon to the end statement

--- config/app.php
+++ config/app.php
@@ -1,8 +1,8 @@
 <?php 
 
 	const APP_URL="http://localhost/sac";
-	const APP_NAME="Sistema Administrativo de Condominios"
-	const APP_SESSION_NAME="SAC"
+	const APP_NAME="Sistema Administrativo de Condominios";
+	const APP_SESSION_NAME="SAC";
 
 	date_default_timezone_set("America/Caracas");
 
