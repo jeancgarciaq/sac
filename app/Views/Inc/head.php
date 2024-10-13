2b22f4976315005feadb28344c40c200871e60e2
jeancgarciaq <jcvictory@hotmail.com>
2024-10-13 17:54:54 -0400

    fix: Change the dir to uppercase

--- app/Views/Inc/head.php
+++ app/Views/Inc/head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Sistema Administrativo de Condominios</title>
-<link rel="stylesheet" href="Css/bulma.min.css">
-<link rel="stylesheet" href="Css/estilos.css">
+<title><?php echo APP_NAME; ?></title>
+<link rel="stylesheet" href="css/bulma.min.css">
+<link rel="stylesheet" href="css/estilos.css">
