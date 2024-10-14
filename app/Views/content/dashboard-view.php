913d4e38d5e58dd2fd39e6255aac8385a309b581
jeancgarciaq <jcvictory@hotmail.com>
2024-10-13 20:18:35 -0400

    fix: change de html is not definitive

--- app/Views/content/dashboard-view.php
+++ app/Views/content/dashboard-view.php
@@ -1,4 +1,17 @@
-<h1 class="title"> Hello World </h1>
-<p class="subtitle">
-	My first website with <strong>Bulma</strong>!
-</p>
+<div class="container is-fluid">
+	<h1 class="title">Tablero</h1>
+  	<div class="columns is-flex is-justify-content-center">
+    	<figure class="image is-128x128">
+    		<?php
+    			/*if(is_file("./app/views/fotos/".$_SESSION['foto'])){
+    				echo '<img class="is-rounded" src="'.APP_URL.'app/views/fotos/'.$_SESSION['foto'].'" alt="Imagen">';
+    			}else{
+    				echo '<img class="is-rounded" src="'.APP_URL.'app/views/fotos/default.png", alt="Imagen">';
+    			}*/
+    		?>
+		</figure>
+  	</div>
+  	<div class="columns is-flex is-justify-content-center">
+  		<h2 class="subtitle">¡Bienvenido Username<?php //echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+  	</div>
+</div>
